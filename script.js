30b5e9d3ce30315afb6216153fc00ed587d31949
Juanita.Pacasmayo <w180693@gmail.com>
2026-08-18 14:39:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const toggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".main-nav");
 
+toggle.addEventListener("click", () => {
+  const isOpen = nav.classList.toggle("open");
+  toggle.setAttribute("aria-expanded", isOpen);
+});
