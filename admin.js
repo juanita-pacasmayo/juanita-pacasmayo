23a176923e27a68fc41353701004c06dd3bcba96
Juanita.Pacasmayo <w180693@gmail.com>
2026-09-13 09:20:07 -0500

    Update admin.js

--- admin.js
+++ admin.js
@@ -10,7 +10,7 @@
 // ==========================================================
 
 const URL_APPS_SCRIPT =
-  "https://script.google.com/macros/s/AKfycbxZW06LP3ctRtIZXBBlo3paILcJCbjQVDMCuOLmNnqU4BuZpbMz3b8jh82V8ZNki1U/exec";
+  "https://script.google.com/macros/s/AKfycbxZW06LP3ctRtIZXBBlo3paILCjcBjQVDMCuOLmNnqU4BuZpbMz3b8jh82V8ZNki1U/exec";
 
 
 // ==========================================================
